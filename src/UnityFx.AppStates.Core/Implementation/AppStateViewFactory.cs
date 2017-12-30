18ebfa903cc226a4850efa4eff26c3151b4b80b7
Alexander Bogarsukov <a.bogarsukov@gmail.com>
2017-12-30 14:44:27 +0200

    added skeleton implementation of IAppStateViewFactory

---
+++ src/UnityFx.AppStates.Core/Implementation/AppStateViewFactory.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace UnityFx.AppStates
+{
+	/// <summary>
+	/// Implementatino of <see cref="IAppStateViewFactory"/>.
+	/// </summary>
+	internal class AppStateViewFactory : MonoBehaviour, IAppStateViewFactory
+	{
+		#region data
+		#endregion
+
+		#region interface
+
+		public void Initialize()
+		{
+		}
+
+		#endregion
+
+		#region MonoBehaviour
+		#endregion
+
+		#region IAppStateViewFactory
+
+		public IAppStateView CreateView(string name, IAppStateView insertAfter)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+		#region implementation
+		#endregion
+	}
+}
