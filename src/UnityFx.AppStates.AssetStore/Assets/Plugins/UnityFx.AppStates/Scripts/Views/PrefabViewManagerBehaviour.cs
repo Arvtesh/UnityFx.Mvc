0e08cb48e4e646345b8ed2803bc888c827c51dcf
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-10-12 17:32:36 +0300

    Added PrefabViewManager

---
+++ src/UnityFx.AppStates.AssetStore/Assets/Plugins/UnityFx.AppStates/Scripts/Views/PrefabViewManagerBehaviour.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using UnityEngine;
+
+namespace UnityFx.AppStates
+{
+	/// <summary>
+	/// Prefab view manager.
+	/// </summary>
+	public class PrefabViewManagerBehaviour : DisposableBehaviour
+	{
+	}
+}
