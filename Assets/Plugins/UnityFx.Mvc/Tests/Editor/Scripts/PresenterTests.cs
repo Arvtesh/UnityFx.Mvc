d447f02cdf8105105dc172c8eea1d9c3aa4b4227
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2019-10-09 11:00:27 +0300

    Added package.json

---
+++ Assets/Plugins/UnityFx.Mvc/Tests/Editor/Scripts/PresenterTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (C) 2019 Alexander Bogarsukov. All rights reserved.
+// See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+namespace UnityFx.Mvc
+{
+	[Category("Presenter"), TestOf(typeof(Presenter))]
+	public class PresenterTests : IDisposable
+	{
+		private Presenter _presenter;
+
+		[SetUp]
+		public void Init()
+		{
+			
+		}
+
+		[TearDown]
+		public void Dispose()
+		{
+			
+		}
+	}
+}
