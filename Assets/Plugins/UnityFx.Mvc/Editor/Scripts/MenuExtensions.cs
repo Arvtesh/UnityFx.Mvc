4e62d30d2cb1daa71d0e24f5afa2d881c86dac4e
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2019-10-30 19:29:30 +0200

    Added viewcontroller creation helper

---
+++ Assets/Plugins/UnityFx.Mvc/Editor/Scripts/MenuExtensions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (C) 2019 Alexander Bogarsukov. All rights reserved.
+// See the LICENSE.md file in the project root for more information.
+
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityFx.Mvc
+{
+	/// <summary>
+	/// Unity menu extensions.
+	/// </summary>
+	public static class MenuExtensions
+	{
+	}
+}
---
+++ Assets/Scripts/Lobby/LobbyController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityFx.Mvc;
+
+namespace Ttt
+{
+	/// <summary>
+	/// LobbyController
+	/// </summary>
+	/// <seealso cref="LobbyView"/>
+	[ViewController(PresentOptions = PresentOptions.None)]
+	public class LobbyController : ViewController<LobbyView>
+	{
+		#region data
+		#endregion
+
+		#region interface
+
+		/// <summary>
+		/// Controller-specific commands.
+		/// </summary>
+		public abstract new class Commands : ViewController.Commands
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="LobbyController"/> class.
+		/// </summary>
+		public LobbyController(IPresentContext context)
+			: base(context)
+		{
+			// TODO: Initialize the controller view here. Add arguments to the Configure() as needed.
+			View.Configure();
+		}
+
+		#endregion
+
+		#region ViewController
+
+		/// <inheritdoc/>
+		protected override bool OnCommand(string commandName, object commandArgs)
+		{
+			// TODO: Process view commands here. See list of commands in Commands.
+			return false;
+		}
+
+		#endregion
+
+		#region implementation
+		#endregion
+	}
+}
