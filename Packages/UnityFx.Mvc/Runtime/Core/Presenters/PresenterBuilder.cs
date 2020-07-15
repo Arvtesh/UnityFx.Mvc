304056c1b679e8dffaa6c27642458ac627e8a72b
abogarsukov <a.bogarsukov@program-ace.com.ua>
2020-07-15 13:19:17 +0300

    Removed IPresenterBuilder

--- Packages/UnityFx.Mvc/Runtime/Core/Presenters/PresenterBuilder.cs
+++ Packages/UnityFx.Mvc/Runtime/Core/Presenters/PresenterBuilder.cs
@@ -9,10 +9,44 @@ using UnityEngine;
 namespace UnityFx.Mvc
 {
 	/// <summary>
-	/// Defines a class that provides the mechanisms to configure a presenter.
+	/// Defines a class that provides the mechanisms to create and configure a presenter.
 	/// </summary>
-	/// <seealso cref="UGUIViewFactoryBuilder"/>
-	public sealed class PresenterBuilder : IPresenterBuilder
+	/// <remarks>
+	/// This interface defines fluent configuration API for <see cref="IPresenter"/>. It is designed to be
+	/// similar to ASP.NET <see href="https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.builder.iapplicationbuilder"/>.
+	/// </remarks>
+	/// <example>
+	/// The following code demonstrates configuring the app presenter:
+	/// <code>
+	/// private void Configure(IPresenterBuilder presenterBuilder)
+	/// {
+	///		presenterBuilder
+	///			.UseViewFactory(_myViewFactory)
+	///			.UseViewControllerFactory(_myCustomControllerFactory)	// If not called, default controller factory is used.
+	///			.UsePresentDelegate((presenter, controllerInfo) =>
+	///				{
+	///					// Log the present operation.
+	///					Debug.Log($"Present {controllerInfo.ControllerType.Name}.");
+	///					return Task.CompletedTask;
+	///				})
+	///			.UsePresentDelegate((presenter, controllerInfo) =>
+	///				{
+	///					// Make sure that user has been authorized.
+	///					var userInfo = (UserInfo)presenter.ServiceProvider.GetService(typeof(UserInfo));
+	///					
+	///					if (!userInfo.IsAuthorized)
+	///					{
+	///						throw new InvalidOperationException();
+	///					}
+	/// 
+	///					return Task.CompletedTask;
+	///				});
+	/// }
+	/// </code>
+	/// </example>
+	/// <seealso cref="IPresenter"/>
+	/// <seealso href="https://en.wikipedia.org/wiki/Builder_pattern"/>
+	public sealed class PresenterBuilder
 	{
 		#region data
 
@@ -32,6 +66,11 @@ namespace UnityFx.Mvc
 		#endregion
 
 		#region interface
+
+		/// <summary>
+		/// Gets the <see cref="IServiceProvider"/> that provides access to the application's service container.
+		/// </summary>
+		public IServiceProvider ServiceProvider => _serviceProvider;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="PresenterBuilder"/> class.
@@ -59,15 +98,17 @@ namespace UnityFx.Mvc
 			_useServiceProvider = useServiceProvider;
 		}
 
-		#endregion
-
-		#region IPresenterBuilder
-
-		/// <inheritdoc/>
-		public IServiceProvider ServiceProvider => _serviceProvider;
-
-		/// <inheritdoc/>
-		public IPresenterBuilder UseViewFactory(IViewFactory viewFactory)
+		/// <summary>
+		/// Sets a <see cref="IViewFactory"/> instace to use. The factory should be set before calling <see cref="Build"/>.
+		/// </summary>
+		/// <param name="viewFactory">A view factory to use.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="viewFactory"/> is <see langword="null"/>.</exception>
+		/// <exception cref="InvalidOperationException">Thrown if a view factory is already set.</exception>
+		/// <seealso cref="UseViewControllerFactory(IViewControllerFactory)"/>
+		/// <seealso cref="UseBindings(IViewControllerBindings)"/>
+		/// <seealso cref="UseEventSource(IPresenterEventSource)"/>
+		/// <seealso cref="Build"/>
+		public PresenterBuilder UseViewFactory(IViewFactory viewFactory)
 		{
 			if (_viewFactory != null)
 			{
@@ -78,8 +119,16 @@ namespace UnityFx.Mvc
 			return this;
 		}
 
-		/// <inheritdoc/>
-		public IPresenterBuilder UseViewControllerFactory(IViewControllerFactory viewControllerFactory)
+		/// <summary>
+		/// Sets a <see cref="IViewControllerFactory"/> instace to use. If not called, a default factory is used.
+		/// </summary>
+		/// <param name="viewControllerFactory">A view controller factory to use.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="viewControllerFactory"/> is <see langword="null"/>.</exception>
+		/// <exception cref="InvalidOperationException">Thrown if a view controller factory is already set.</exception>
+		/// <seealso cref="UseBindings(IViewControllerBindings)"/>
+		/// <seealso cref="UseViewFactory(IViewFactory)"/>
+		/// <seealso cref="Build"/>
+		public PresenterBuilder UseViewControllerFactory(IViewControllerFactory viewControllerFactory)
 		{
 			if (_viewControllerFactory != null)
 			{
@@ -90,8 +139,16 @@ namespace UnityFx.Mvc
 			return this;
 		}
 
-		// <inheritdoc/>
-		public IPresenterBuilder UseBindings(IViewControllerBindings viewControllerBindings)
+		/// <summary>
+		/// Sets a <see cref="IViewControllerBindings"/> instace to use. If not called, a default factory is used.
+		/// </summary>
+		/// <param name="viewControllerBindings">A view/controller bindings to use.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="viewControllerBindings"/> is <see langword="null"/>.</exception>
+		/// <exception cref="InvalidOperationException">Thrown if a view controller binding is already set.</exception>
+		/// <seealso cref="UseViewControllerFactory(IViewControllerFactory)"/>
+		/// <seealso cref="UseViewFactory(IViewFactory)"/>
+		/// <seealso cref="Build"/>
+		public PresenterBuilder UseBindings(IViewControllerBindings viewControllerBindings)
 		{
 			if (_viewControllerBindings != null)
 			{
@@ -102,8 +159,15 @@ namespace UnityFx.Mvc
 			return this;
 		}
 
-		/// <inheritdoc/>
-		public IPresenterBuilder UseEventSource(IPresenterEventSource eventSource)
+		/// <summary>
+		/// Sets an event source instance to use. If not called, a default provider is used.
+		/// </summary>
+		/// <param name="eventSource">An event source to use.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="eventSource"/> is <see langword="null"/>.</exception>
+		/// <exception cref="InvalidOperationException">Thrown if an event source is already set.</exception>
+		/// <seealso cref="UseViewFactory(IViewFactory)"/>
+		/// <seealso cref="Build"/>
+		public PresenterBuilder UseEventSource(IPresenterEventSource eventSource)
 		{
 			if (_eventSource != null)
 			{
@@ -114,8 +178,13 @@ namespace UnityFx.Mvc
 			return this;
 		}
 
-		/// <inheritdoc/>
-		public IPresenterBuilder UsePlayerLoop()
+		/// <summary>
+		/// Sets a <see cref="UnityEngine.LowLevel.PlayerLoop"/>-based event source. Requires Unity 2019.3 or newer.
+		/// </summary>
+		/// <exception cref="InvalidOperationException">Thrown if an event source is already set, either with this method or with <see cref="UseEventSource(IPresenterEventSource)"/>.</exception>
+		/// <seealso cref="UseEventSource(IPresenterEventSource)"/>
+		/// <seealso cref="Build"/>
+		public PresenterBuilder UsePlayerLoop()
 		{
 			if (_eventSource != null)
 			{
@@ -126,8 +195,14 @@ namespace UnityFx.Mvc
 			return this;
 		}
 
-		/// <inheritdoc/>
-		public IPresenterBuilder UsePresentDelegate(PresentDelegate presentDelegate)
+		/// <summary>
+		/// Adds a <see cref="PresentDelegate"/> to presenter middleware chain.
+		/// </summary>
+		/// <param name="presentDelegate">The middleware to add.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="presentDelegate"/> is <see langword="null"/>.</exception>
+		/// <seealso cref="UseErrorDelegate(Action{Exception})"/>
+		/// <seealso cref="Build"/>
+		public PresenterBuilder UsePresentDelegate(PresentDelegate presentDelegate)
 		{
 			if (presentDelegate is null)
 			{
@@ -143,7 +218,14 @@ namespace UnityFx.Mvc
 			return this;
 		}
 
-		public IPresenterBuilder UseErrorDelegate(Action<Exception> errorDelegate)
+		/// <summary>
+		/// Adds an error handler.
+		/// </summary>
+		/// <param name="errorDelegate">The delegate to be called on errors.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="errorDelegate"/> is <see langword="null"/>.</exception>
+		/// <seealso cref="UsePresentDelegate(PresentDelegate)"/>
+		/// <seealso cref="Build"/>
+		public PresenterBuilder UseErrorDelegate(Action<Exception> errorDelegate)
 		{
 			if (errorDelegate is null)
 			{
@@ -154,7 +236,11 @@ namespace UnityFx.Mvc
 			return this;
 		}
 
-		/// <inheritdoc/>
+		/// <summary>
+		/// Builds a <see cref="IPresenter"/> instance.
+		/// </summary>
+		/// <exception cref="InvalidOperationException">Thrown if presenter cannot be constructed (for instance, <see cref="IViewFactory"/> is not set and cannot be located).</exception>
+		/// <seealso cref="UseViewFactory(IViewFactory)"/>
 		public IPresentService Build()
 		{
 			if (_presenter is null)
