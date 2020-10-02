33ce2b73c77fd05aa65cd3a5d74dae3e3ec2ea22
Arvtesh <22732458+Arvtesh@users.noreply.github.com>
2020-10-03 01:02:30 +0300

    Added IAsyncPresentable and IAsyncDismissable interfaces

--- Packages/UnityFx.Mvc/Runtime/Abstractions/Core/IPresenter.cs
+++ Packages/UnityFx.Mvc/Runtime/Abstractions/Core/IPresenter.cs
@@ -21,8 +21,6 @@ namespace UnityFx.Mvc
 		/// </summary>
 		/// <param name="controllerType">Type of the view controller to present.</param>
 		/// <param name="args">Controller arguments.</param>
-		/// <param name="presentOptions">Present flags.</param>
-		/// <param name="parent">Parent transform for the view (or <see langword="null"/>).</param>
 		/// <returns>An object that can be used to track the operation propress.</returns>
 		/// <exception cref="ArgumentNullException">Thrown if <paramref name="controllerType"/> is <see langword="null"/>.</exception>
 		/// <exception cref="ArgumentException">Thrown if <paramref name="controllerType"/> cannot be used to instantiate the controller (for instance, it is abstract type).</exception>
